178e3c1ce3cb74875f2327196222cec1e84a809f
fjams7 <123623477+fjams7@users.noreply.github.com>
2023-04-01 02:21:48 +0800

    Update account.php

--- account.php
+++ account.php
@@ -1,18 +1,11 @@
 <html>
-    <title> Login Form </title>
+<title> Account </title>
 <head>
     <style>
         html {font-family:"century gothic";}
         body {text-align:center; background-color: #E4D0D0;}
-        input {
-            width: 400px;
-            padding: 0.1px 10px;
-            margin: 4px 0;
-            box-sizing: border-box;
-            border: 1px solid gray;
-            border-radius: 6px;
-            font-size: 20px;
-        }
+        h3 {font-size: 35px;}
+        p {font-size: 20px;}
         input[type=submit] {
             width: 100px;
             padding: 2px 4px;
@@ -23,34 +16,31 @@
             font-size: 20px;
             background-color: #FFFFFF;
         }
-        label {
-            display: inline-block;
-            width: 400px;
-            text-align: left;
-        }
-        h1 {font-size: 30px;}
     </style>
 </head>
-    <h1>
-        PHP FORM ACTIVITY <br>
-    </h1>
-    <p> Jamito, Francis - IPT 101B </p>
-    <body>
-        <p>
-            <hr><br>
-        <form action="account.php" method="POST">
-            <label for="fname">First Name</label> <br>
-                <input type="text" name="fname" id="fname" required> <br><br>
-            <label for="fname">Last Name</label> <br>
-                <input type="text" name="sname" id="sname" required> <br><br>
-            <label for="email">Email Address</label> <br>
-                <input type="email" name="email" id="email" maxlength="30" required> <br><br>
-            <label for="pwd">Password</label> <br>
-                <input type="password" name="pwd" id="pwd" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" maxlength="30" title="Must contain at least one number,one uppercase and lowercase letter, and at least 8 or more characters" required> <br><br>
-            <label for="cpwd">Confirm Password</label> <br>
-                <input type="password" name="cpwd" id="cpwd" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" maxlength="30" title="Must contain at least one number,one uppercase and lowercase letter, and at least 8 or more characters" required> <br><br>
-                <br><br><input type="submit" value="Submit">
-        </form>
-        </p>
-    </body>
+<body>
+    <p>
+        <?php
+        if (!empty($_POST)) {
+            $name = $_POST["fname"].' '.$_POST["sname"];
+            $email = $_POST["email"];
+            $len = strlen($_POST["pwd"]);
+            echo ("<h3>Hello, $name <hr></h3>");
+            echo ("Your email is: <b> $email </b> <br>");
+            echo ("Your password has <b> $len characters </b> <br><br>");
+                if ($_POST["pwd"] != $_POST["cpwd"]) {
+                    echo("Passwords did not match.");
+                } else {
+                    echo("Passwords matched.");
+                }
+            } else {
+                echo("<br><br><br><br><br><br><br><br><br><br>");
+                echo("<h1><br>INVALID DATA</h1>");
+            }
+        ?>
+    </p>
+        <form action="submitform.php"> <br>
+        <input type="submit" value="Go Back">
+    </style>
+</body>
 </html>
