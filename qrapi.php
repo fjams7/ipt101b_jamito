41b02db2a009426745084e4f15759f26cc2acf04
fjams7 <jamito.francis_john_angelo@hnu.edu.ph>
2023-05-09 23:45:01 +0800

    no

--- qrapi.php
+++
@@ -1,9 +0,0 @@
-<title> QR Code </title>
-<?php
-$url = $_POST["link"];
-$apiUrl = "https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=".urlencode($url);
-$response = file_get_contents($apiUrl);
-    echo "<html><head><style>body {background-color: #FFFFFF;} .qr-code-container {display: flex; justify-content: center; align-items: center; height: 100vh;}</style></head><body>";
-    echo "<div class='qr-code-container'><img src='data:image/png;base64,".base64_encode($response)."'/></div>";
-    echo "</body></html>";
-?>
